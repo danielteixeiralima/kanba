f706d759893d7674b5d401113d2f67ebab79460f
unknown <fgomesc0586@gmail.com>
2023-05-30 12:46:15 -0300

    Mudança no main.js

--- static/main.js
+++ static/main.js
@@ -33,12 +33,9 @@ $(document).ready(function(){
     });
 });
 
-
-
 function updateObjectives() {
-var objetivoOriginal = $('#objetivo').data('original');
+    var objetivoOriginal = $('#objetivo').data('original');
     var empresaId = $('#empresa').val();
-
 
     $.getJSON('/get_okrs/' + empresaId, function(data) {
         var select = $('#objetivo');
@@ -52,33 +49,6 @@ var objetivoOriginal = $('#objetivo').data('original');
             }
             select.append(option);
         });
-        // Atualize o campo oculto 'id_okr' para refletir o primeiro objetivo na lista, a menos que um objetivo original esteja definido
-        if (objetivoOriginal) {
-            $('#id_okr').val(objetivoOriginal);
-        } else {
-            $('#id_okr').val($('#objetivo').val());
-        }
-    });
-    $.getJSON('/get_empresa_info/' + empresaId, function(data) {
-        var select = $('#objetivo');
-        select.empty();
-        data.objetivos.forEach(function(objetivo) {
-            var option = $('<option>');
-            option.val(objetivo.id);  // Define o valor da opção para o ID do objetivo
-            option.text(objetivo.texto);
-            if (objetivo.id == objetivoOriginal) {
-                option.prop('selected', true);
-            }
-            select.append(option);
-        });
-
-        // Agora, também atualizamos as outras partes da página
-        $('#descricao_div').text(data.descricao_empresa);
-        $('#cargos_div').text(data.usuarios.join(', '));
-        $('#okrs_div').text(data.objetivos.join(', '));
-        $('#krs_div').text(data.krs.join(', '));
-        $('#macroacoes_div').text(data.macro_acoes.join(', '));
-
         // Atualize o campo oculto 'id_okr' para refletir o primeiro objetivo na lista, a menos que um objetivo original esteja definido
         if (objetivoOriginal) {
             $('#id_okr').val(objetivoOriginal);
